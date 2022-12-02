c31a97abada9530e5fdd206264de6d9382a41a7d
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-12-02 16:27:40 +0200

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,53 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Link, createTheme, ThemeProvider } from "@mui/material";
+import { Suspense } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+const theme = createTheme({
+    palette: {
+        secondary: {
+            main: '#fff',
+        },
+    },
+});
+
 export default function Header () {
     return (
-        <AppBar>
-            <Toolbar >
-                <Typography>Home</Typography>
-                <Typography>Register</Typography>
-                <Typography>Log in</Typography>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="static">
+                <Toolbar component="nav">
+                    <ThemeProvider theme={theme}>
+                        <Link
+                            component={NavLink}
+                            variant="h6"
+                            underline="none"
+                            color="secondary"
+                            sx={{ flexGrow: 1 }}
+                            to="/"
+                            end>
+                            Home
+                        </Link>
+                        <Link
+                            component={NavLink}
+                            variant="h6"
+                            underline="none"
+                            color="secondary"
+                            sx={{ flexGrow: 0.05 }}
+                            to="/register">
+                            Register
+                        </Link>
+                        <Link
+                            component={NavLink}
+                            variant="h6"
+                            underline="none"
+                            color="secondary"
+                            to="/login">
+                            Log in
+                        </Link>
+                    </ThemeProvider>
+                </Toolbar>
+            </AppBar>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Outlet/>
+            </Suspense>
+        </>   
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 export default function Home() {
     return (
         <Box>
-            <Typography variant="h1">Hello</Typography>
+            <Typography variant="h1">Welcome to Phonebook</Typography>
         </Box>
     )
 }
