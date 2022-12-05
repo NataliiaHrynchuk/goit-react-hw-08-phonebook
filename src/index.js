0ff9b57ae860720557b4109a82ac7e51f93c8de8
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-12-05 11:09:27 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { store, persistor } from 'redux/store';
 import { App } from 'App';
 import './index.css';
 import '@fontsource/roboto/300.css';
@@ -10,9 +12,11 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <Provider>
+  <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
   <BrowserRouter basename="goit-react-hw-08-phonebook">
     <App />
-  </BrowserRouter>
-  // </Provider>
+    </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
